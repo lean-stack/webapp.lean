0efc8b0010dd5e1cdb9b4f5e14132e73cd2447a5
Michael Alt <lean@michael-alt.info>
2014-11-14 19:39:42 +0100

    v0.0.2: Handling the application sass files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,47 @@
-(function(require){
-  'use strict';
 
-  var gulp = require('gulp');
+var gulp = require('gulp');
+
+// load plugins into $
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function(){
+
+  return gulp.src('src/styles/app.scss')
+    .pipe($.sass())
+    .pipe($.autoprefixer())
+    .pipe(gulp.dest('src/styles'))
+    .pipe($.size())
+    .pipe($.notify("Styles preprocessing complete."));
+});
+
+gulp.task('build', ['styles']);
+
+gulp.task('default', ['build']);
+
+gulp.task('dist', ['build'], function () {
+
+  $.notify({onLast: true});
+  var assets = $.useref.assets();
+
+  return gulp.src('src/*.html')
+
+      .pipe(assets)
+      .pipe($.if('*.css',$.csso()))
+      .pipe(assets.restore())
+
+      .pipe($.useref())
+      .pipe(gulp.dest('dist'))
+      .pipe($.size())
+      .pipe($.notify({onLast: true, message: "Dist build complete."}));
+});
+
+// Task is not involved in build task.
+// Won't work with no bower depencies.
+gulp.task('wiredep', function(){
+
   var wiredep = require('wiredep').stream;
+  return gulp.src('src/index.html')
+    .pipe(wiredep())
+    .pipe(gulp.dest('src'));
 
-  gulp.task('wiredep', function(){
-    gulp.src('./app/index.html')
-      .pipe(wiredep())
-      .pipe(gulp.dest('./app'));
-  });
-})(require);
+});

b9b62aed5cfe4dba7e6ce143b6eb95b65150758c
Michael Alt <lean@michael-alt.info>
2014-11-15 11:12:19 +0100

    Debug mode.

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('scripts', function(){
   var browserify = require('browserify');
   var source     = require('vinyl-source-stream');
 
-  browserify(['./src/scripts/app/main.js'])
+  browserify({entries: ['./src/scripts/app/main.js'], debug: true})
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('src/scripts'))
